70d10117299267e4c542d1a4d7d3cb46ae92e2be
Thanesh Pannirselvam <t.pannirselvam@fdmgroup.com>
2022-01-19 21:03:46 +1100

    add helper functions for sending sms and email alert

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from helpers import *
 def main():
     web_driver = initialise_webdriver()
     
-    fill_select_options(web_driver)
-    select_date_input(web_driver)
+    prefill_select_options(web_driver)
+    click_date_input(web_driver)
     find_available_date(web_driver, list_of_months=get_all_months(web_driver))
     display_completion_time()
 
